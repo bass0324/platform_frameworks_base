2f96ffe52f57c223308187235aa7bd88584e05a3
John Basista <03bass23@gmail.com>
2015-01-20 17:19:08 -0500

    Revert "volume key adjust sound"
    This reverts commit 5ecf0b6addf579752e86e6cdc15a48f0824e86eb.

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -47,7 +47,6 @@ import android.os.Debug;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.KeyEvent;
@@ -1210,12 +1209,6 @@ public class VolumePanel extends Handler {
     }
 
     protected void onPlaySound(int streamType, int flags) {
-        
-        // Volume adjust sound
-        if (Settings.System.getInt(mContext.getContentResolver(),
-             Settings.System.VOLUME_KEY_ADJUST_SOUND, 1) == 0) {
-             return;
-        }
 
         if (hasMessages(MSG_STOP_SOUNDS)) {
             removeMessages(MSG_STOP_SOUNDS);
